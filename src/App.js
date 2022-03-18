f1ab97deb2e40a10ffe15dc8b33470c61f9fef83
somm12 <thals8744@naver.com>
2022-03-18 15:09:45 +0900

    react todo list

--- src/App.js
+++ src/App.js
@@ -2,42 +2,38 @@ import Button from "./Button";
 import styles from "./App.module.css";
 import { useEffect, useState } from "react";
 
-//어떻게 특정 코드들이 첫번째 component render에서만 실행되게 할까
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => {
-    setValue((prev) => prev + 1);
-  };
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => {
-    setKeyword(event.target.value);
+    setToDo(event.target.value);
   };
-  //state가 변할 때 마다 react는 rerende를 해준다. 그래서 굳이 rerender가 필요없는 코드는 useEffect를 이용해서 한번만 실행 할 수 있게함.
-  console.log("I run all the time");
-  useEffect(() => {
-    console.log("call the API..");
-  }, []);
-  // then except run once, what should I do? If I want run specific code in specific situation like some value changes.
-  useEffect(() => {
-    console.log("I run only once");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run if counter changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run if counter & keyword changes");
-  }, [keyword, counter]);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      <h1 className={styles.title}>{counter}</h1>
-      <input
-        value={keyword}
-        type="text"
-        placeholder="Search Here.."
-        onChange={onChange}
-      />
-      <button onClick={onClick}>Click me</button>
-      <Button text={"continue"} />
+      <h1>My ToDos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write to do "
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
